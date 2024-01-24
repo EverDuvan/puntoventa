2b6d128b97581eaade39d57f18c061cee91401cc
everduvan <everduvanhdez@gmail.com>
2024-01-23 19:17:52 -0500

    update

--- clientes/urls.py
+++ clientes/urls.py
@@ -2,7 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.clientes_view, name='clientes'),
     path('clientes/', views.clientes_view, name='clientes'),
     path('add_clientes', views.add_cliente_view, name='AddCliente'),
     path('edit_clientes', views.edit_cliente_view, name='EditCliente'),
